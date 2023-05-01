bb75771e2417ec06586a1a26a5b77ea0e023d83d
DDing <jangmee9918@gmail.com>
2023-05-01 23:34:50 +0900

    oauth 구현

--- oauth.js
+++ oauth.js
@@ -1,7 +1,9 @@
 const { AuthorizationCode } = require('simple-oauth2');
 const app = require('express')();
-const fs=require('fs');
+const Request = require('request');
+const fs = require('fs');
 const callbackUrl = 'http://127.0.0.1:3000/callback';
+const getURL = `https://api.fitbit.com/1.2/user/-/`;
 const client = new AuthorizationCode({
     client: {
         id: '23QSN4',
@@ -18,7 +20,7 @@ const client = new AuthorizationCode({
 const authorizationUri = client.authorizeURL({
     scope: 'activity cardio_fitness electrocardiogram heartrate location nutrition oxygen_saturation profile respiratory_rate settings sleep social temperature weight',
     redirect_uri: callbackUrl,
-}).replace('api','www');
+}).replace('api', 'www');
 
 app.get('/auth', (req, res) => {
     console.log(authorizationUri);
@@ -28,12 +30,12 @@ app.get('/callback', async (req, res) => {
     const { code } = req.query;
     const options = {
         code,
-        redirect_uri:callbackUrl,
+        redirect_uri: callbackUrl,
     };
     try {
         const accessToken = await client.getToken(options);
-        fs.writeFile('AccessToken.json',fs.accessToken.token,(err)=>{
-            if(err){
+        fs.writeFile('AccessToken.json', JSON.stringify(accessToken.token), (err) => {
+            if (err) {
                 console.error(err);
                 return;
             }
@@ -50,9 +52,32 @@ app.get('/callback', async (req, res) => {
 app.get('/', (req, res) => {
     res.send('Hello<br><a href="/auth">Fitbit API</a>');
 });
-app.get('/getdata',(req,res)=>{
-    fs.readFile('AccessToken.json','utf-8',function(err,data){
-        console.log(data);
+app.get('/getdata', (req, res) => {
+    fs.readFile('AccessToken.json', 'utf-8', function (err, data) {
+        const accessToken = JSON.parse(data);
+        console.log(req.query.request_json);
+        var getdata = new Promise((resolve, reject) => {
+            Request({
+                url: getURL + req.query.request_json,
+                method: 'GET',
+                headers: {
+                    Authorization: 'Bearer ' + accessToken.access_token
+                },
+                json: true
+            }, (error, response, body) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve([
+                        body,
+                        response
+                    ]);
+                }
+            });
+        }).then(results=>{
+            res.send(results[0]);
+        });
+
     });
 })
 app.listen(3000, () => {
